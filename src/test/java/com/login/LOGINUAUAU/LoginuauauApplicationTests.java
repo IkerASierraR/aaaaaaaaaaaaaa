8a5df34cdcaaf2d77514e592b9176298045fe25c
IkerASierraR <is2023077090@virtual.upt.pe>
2025-11-07 13:20:06 -0500

    version 1

---
+++ src/test/java/com/login/LOGINUAUAU/LoginuauauApplicationTests.java
@@ -0,0 +1,13 @@
+package com.login.LOGINUAUAU;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginuauauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
